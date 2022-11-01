8d7f464a4bed180887080036423783170180fcef
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-11-01 20:58:36 +0530

    project init
    -removed boiler plate code from app.jsx
    -added image assets
    -modified tailwind.config.js

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <h1 className="text-primaryRed">Hello from react!! </h1>;
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "780px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      // primary colors
+      primaryRed: "#f65351",
+      primaryBlue: " #0a317b",
+
+      // neutral colors
+      nWhite: "#ffffff",
+      nGrayishBlue100: "#f7fafd",
+      nGrayishBlue200: "#e5effa",
+      nGrayishBlue300: "#dde7ee",
+      nGrayishBlue400: "#939dae",
+      nGrayishBlue500: "#5e6778",
+      nVeryDarkBlue: "#1c202b",
+    },
+    extend: {
+      fontFamily: {
+        body: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      width: {
+        vw: "90vw",
+        fixed: "450px",
+        mFixed: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
